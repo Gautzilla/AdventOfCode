a77da2d6f685c2e2194aa28a85849d3522f8c614
Gautzilla <gauthier.berthomieu@gmail.com>
2021-12-10 17:33:19 +0100

    Added day 1

---
+++ AdventOfCode2021/Day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    class Day1
+    {
+        static public void Solve(int part)
+        {
+            // string path = @"..\..\Inputs\day4Example.txt";
+            string path = @"..\..\Inputs\day1.txt";
+
+            List<int> input = File.ReadAllLines(path).Select(a => int.Parse(a)).ToList();
+
+            int increases = 0;
+
+            if (part == 1)
+            {
+                for (int i = 1; i < input.Count(); i++)
+                {
+                    if (input[i] > input[i - 1]) increases++;
+                }
+            } else
+            { 
+                for (int i = 3; i < input.Count(); i++)
+                {
+                    if (input[i] + input[i-1] + input[i-2] > input[i - 1] + input[i - 2] + input[i - 3]) increases++;
+                }
+            }
+            Console.WriteLine(increases);
+        }
+    }
+}
