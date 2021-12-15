69fff78df5791ff67b3beb1b3058a78d43e3b024
Gautzilla <gauthier.berthomieu@gmail.com>
2021-12-16 00:44:26 +0100

    Added day15 part 2, still needs optimization.

--- AdventOfCode2021/Day15.cs
+++ AdventOfCode2021/Day15.cs
@@ -15,97 +15,113 @@ namespace AdventOfCode2021
             public int y { get; }
             public int risk { get; }
             public int cumulateRisk { get; set; }
+            public bool visited { get; set; }
             public Node(int[] coord, int risk)
             {
                 x = coord[0];
                 y = coord[1];
                 this.risk = risk > 9 ? risk % 9 : risk;
                 this.cumulateRisk =  x == 0 && y == 0 ? 0 : int.MaxValue;
+                visited = x == 0 && y == 0;
             }
         }
 
         public static void Solve(int part)
         {
-            string filePath = @"..\..\Inputs\day15Example.txt";
-            //string filePath = @"..\..\Inputs\day15.txt";
+            //string filePath = @"..\..\Inputs\day15Example.txt";
+            string filePath = @"..\..\Inputs\day15.txt";
 
             List<string> input = File.ReadAllLines(filePath).ToArray().ToList();
 
-            List<Node> cavern = new List<Node>();
+            Node[,] cavern = new Node[input[0].Length, input.Count()];
 
-            for (int y = 0; y < input.Count(); y++) for (int x = 0; x < input[0].Length; x++) cavern.Add(new Node(new int[] { x, y }, int.Parse(input[y][x].ToString())));
+            for (int y = 0; y < input.Count(); y++) for (int x = 0; x < input[0].Length; x++) cavern[x,y] = new Node(new int[] { x, y }, int.Parse(input[y][x].ToString()));
 
-            cavern = RepeatCavern(cavern, 4);
+            int repeat = part == 1 ? 1 : 5;
 
-            int shortestPath = ShortestPath(cavern, new int[] { 0, 0 }, new int[] { cavern.Max(n => n.x), cavern.Max(n => n.y)});
+            cavern = RepeatCavern(cavern, repeat);
 
+            int shortestPath = ShortestPath(cavern, new int[] { 0, 0 }, new int[] { cavern.GetLength(0) - 1, cavern.GetLength(1) - 1 });
 
             Console.WriteLine(shortestPath);
         }
 
-        private static void VisualizeCavern (List<Node> cavern, bool risk)
+        private static void VisualizeCavern (Node[,] cavern, bool risk)
         {
-            for (int y = 0; y <= cavern.Max(n => n.y); y++)
+            for (int y = 0; y < cavern.GetLength(1); y++)
             {
-                for (int x = 0; x <= cavern.Max(n => n.x); x++)
+                for (int x = 0; x < cavern.GetLength(0); x++)
                 {
-                    if (risk) Console.Write($"{cavern.First(n => n.x == x && n.y == y).risk}");
-                    else Console.Write($"{cavern.First(n => n.x == x && n.y == y).cumulateRisk} ");
+                    if (risk) Console.Write($"{cavern[x,y].risk}");
+                    else Console.Write($"[{cavern[x,y].cumulateRisk}] ");
                 }
                 Console.Write("\n");
             }
         }
 
-        private static int ShortestPath(List<Node> cavern, int[] startCoord, int[] endCoord)
+        private static int ShortestPath(Node[,] cavern, int[] startCoord, int[] endCoord)
         {
             List<Node> nodesToEvaluate = new List<Node>();
-            List<Node> evaluatedNodes = new List<Node>();
 
-            nodesToEvaluate.Add(cavern.First(n => n.x == startCoord[0] && n.y == startCoord[1]));
+            nodesToEvaluate.Add(cavern[startCoord[0], startCoord[1]]);
 
             while (nodesToEvaluate.Count() > 0)
             {
                 Node activeNode = nodesToEvaluate.First();
-                evaluatedNodes.Add(activeNode);
 
-                for (int i = -1; i <= 1; i++)
+                foreach (Node neighbour in GetNeighbours(cavern, activeNode))
                 {
-                    for (int j = -1; j <= 1; j++)
+                    if (!neighbour.visited)
                     {
-                        if ( (Math.Abs(i) + Math.Abs(j) == 1) && cavern.Any(n => n.x == activeNode.x +i && n.y == activeNode.y + j))
-                        {
-                            Node nodeToEvaluate = cavern.First(n => n.x == activeNode.x + i && n.y == activeNode.y + j);
+                        nodesToEvaluate.Add(neighbour);
+                        neighbour.visited = true;
+                    }
 
-                            if (!evaluatedNodes.Contains(nodeToEvaluate) && !nodesToEvaluate.Contains(nodeToEvaluate)) nodesToEvaluate.Add(nodeToEvaluate);
+                    if (activeNode.cumulateRisk + neighbour.risk < neighbour.cumulateRisk)
+                    {
+                        neighbour.cumulateRisk = activeNode.cumulateRisk + neighbour.risk;
 
-                            if (activeNode.cumulateRisk + nodeToEvaluate.risk < nodeToEvaluate.cumulateRisk) nodeToEvaluate.cumulateRisk = activeNode.cumulateRisk + nodeToEvaluate.risk ;
-                        }
+                        foreach (Node newNeighbour in GetNeighbours(cavern, neighbour)) if (newNeighbour != activeNode) newNeighbour.visited = false; // Ameliorations can be made here to gain efficiency
                     }
                 }
-
-                nodesToEvaluate.RemoveAll(n => n == activeNode);
+                nodesToEvaluate.RemoveAt(0);
             }
-
-            return cavern.First(n => n.x == endCoord[0] && n.y == endCoord[1]).cumulateRisk;
+            return cavern[endCoord[0],endCoord[1]].cumulateRisk;
         }
 
-        private static List<Node> RepeatCavern (List<Node> cavern, int repeatTimes)
+        private static Node[,] RepeatCavern (Node[,] cavern, int repeatTimes)
         {
-            List<Node> newCavern = new List<Node>(cavern);
+            Node[,] newCavern = new Node[ cavern.GetLength(0) * repeatTimes , cavern.GetLength(1) * repeatTimes];
 
-            for (int x = 0; x <= repeatTimes; x++)
+            for (int x = 0; x < repeatTimes; x++)
             {
-
-                for (int y = 0; y <= repeatTimes; y++)
+                for (int y = 0; y < repeatTimes; y++)
                 {
                     foreach (Node node in cavern)
                     {
-                        newCavern.Add(new Node(new int[] { node.x + (cavern.Max(n => n.x) + 1) * x, node.y + (cavern.Max(n => n.y) + 1) * y }, node.risk + x + y));
+                        newCavern[node.x + cavern.GetLength(0) * x , node.y + cavern.GetLength(1) * y] = new Node(new int[] { node.x + cavern.GetLength(0) * x, node.y + cavern.GetLength(1) * y }, node.risk + x + y);
                     }
                 }
             }
 
             return newCavern;
+        }
+
+        private static List<Node> GetNeighbours(Node[,] cavern, Node node)
+        {
+            List<Node> neighbours = new List<Node>();
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if ((Math.Abs(i) + Math.Abs(j) == 1) && node.x + i >= 0 && node.x + i < cavern.GetLength(0) && node.y + j >= 0 && node.y + j < cavern.GetLength(1))
+                    {
+                        neighbours.Add(cavern[node.x + i , node.y + j]);
+                    }
+                }
+            }
+            return neighbours;
         }
     }
 }
