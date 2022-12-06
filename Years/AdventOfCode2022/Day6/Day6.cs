51a9d2bec7b9921b73563b5bf421df05d7470516
Gautzilla <gauthier.berthomieu@gmail.com>
2022-12-06 15:35:42 +0100

    Add day 6

---
+++ Years/AdventOfCode2022/Day6/Day6.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace AdventOfCode2022
+{
+    public static class Day6
+    {
+        public static void Solve(int part)
+        { 
+            string input = File.ReadAllText(@"Day6\input.txt");
+            int bufferSize = part == 1 ? 4 : 14;
+
+            for (int i = 0; i < input.Length-bufferSize; i++)
+            {
+                if (input.Skip(i).Take(bufferSize).GroupBy(c => c).Count() == bufferSize)
+                {
+                    Console.WriteLine(i+bufferSize);
+                    return;
+                }
+            }
+        }
+    }
+}
