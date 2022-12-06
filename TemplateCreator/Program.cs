f39a2513d46fae814e4ec539650d28bc53c75a30
Gautzilla <gauthier.berthomieu@gmail.com>
2022-12-06 12:35:30 +0100

    Add template creator

---
+++ TemplateCreator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace TemplateCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year:");
+            string year = Console.ReadLine();
+            Console.WriteLine("Day:");
+            string day = Console.ReadLine();
+
+            string dayTemplate = File.ReadAllText(@"Template.cs");
+            dayTemplate = Regex.Replace(dayTemplate, "AdventOfCode", $"AdventOfCode{year}");
+            dayTemplate = Regex.Replace(dayTemplate, "Day", $"Day{day}");
+
+            string directoryPath = @$"..\\Years\\AdventOfCode{year}\\Day{day}";
+            Directory.CreateDirectory(directoryPath);
+            File.WriteAllText(directoryPath + @$"\\Day{day}.cs", dayTemplate);
+            File.Create(directoryPath + @"\\input.txt");
+        }
+    }
+}
---
+++ Years/AdventOfCode2022/Day5/Day5.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace AdventOfCode2022
+{
+    public static class Day5
+    {
+        public static void Solve(int part)
+        { 
+            
+        }
+    }
+}
