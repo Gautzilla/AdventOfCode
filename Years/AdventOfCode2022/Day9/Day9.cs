d21b89d4f4e87b039a8261ecf9af4b6161b6ef1a
Gautzilla <gauthier.berthomieu@gmail.com>
2022-12-10 19:01:26 +0100

    Add part 2

--- Years/AdventOfCode2022/Day9/Day9.cs
+++ Years/AdventOfCode2022/Day9/Day9.cs
@@ -2,64 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Text;
-using System.Diagnostics;
 
 namespace AdventOfCode2022
 {
     public static class Day9
     {
-        static (int x, int y) tail = (0,0);
-        static (int x, int y) head = (0,0);
-        static readonly Dictionary<string, (int x, int y)> _directions = new Dictionary<string, (int x, int y)>()
+        static private (int x, int y)[] _knots = new(int x, int y)[0];
+        static private readonly Dictionary<string, (int x, int y)> _directions = new Dictionary<string, (int x, int y)>()
         {
             {"R", (1,0)},
             {"L", (-1,0)},
-            {"U", (0,1)},
-            {"D", (0,-1)},
+            {"U", (0,-1)},
+            {"D", (0,1)},
         };
-        static HashSet<(int x, int y)> visitedSquares = new();
+        static private HashSet<(int x, int y)> _visitedSquares = new();
 
         public static void Solve(int part)
         { 
             (string direction, int number)[] instructions = File.ReadAllLines(@"Day9\input.txt").Select(line => (line.Split(" ").First(), int.Parse(line.Split(" ").Last()))).ToArray();
-            visitedSquares.Add((0,0));
+            
+            _knots = part == 1 ? new (int,int)[2] : new (int,int)[10];
+            
+            _visitedSquares.Add((0,0));
             
             foreach (var instruction in instructions) ManageInstruction(instruction);
             
-            Console.WriteLine(visitedSquares.Count());
+            Console.WriteLine(_visitedSquares.Count());
         }
 
         private static void ManageInstruction((string direction, int number) instruction)
         {
             for (int inst = 0; inst < instruction.number; inst++)
             {
-                MoveHead(instruction.direction);
-                MoveTail();
-                visitedSquares.Add(tail);
+                _knots[0] = _knots[0].MoveHead(instruction.direction);
+                
+                for (int knot = 1; knot < _knots.Length; knot++ ) _knots[knot] = _knots[knot].Follow(_knots[knot-1]);
+                
+                _visitedSquares.Add(_knots.Last());
             }
         }
 
-        private static void MoveHead(string direction)
+        private static (int x, int y) MoveHead(this (int x, int y) head, string direction) => (head.x + _directions[direction].x, head.y + _directions[direction].y);
+
+        private static (int x, int y) Follow(this (int x, int y) knot, (int x, int y) previousKnot)
         {
-                head.x += _directions[direction].x;
-                head.y += _directions[direction].y;
-        }
+            int x = knot.x;
+            int y = knot.y;
 
-        private static void MoveTail()
-        {
-            if (Math.Abs(head.x - tail.x) > 1)
+            if (Math.Abs(previousKnot.x - knot.x) > 1)
             {
-                tail.x += head.x > tail.x ? 1 : -1;
-                tail.y += head.y != tail.y ? (head.y > tail.y ? 1 : -1) : 0;
+                x += previousKnot.x > knot.x ? 1 : -1;
+                y += previousKnot.y != knot.y ? (previousKnot.y > knot.y ? 1 : -1) : 0;
+                return (x,y);
             }
-            if (Math.Abs(head.y - tail.y) > 1)
+            if (Math.Abs(previousKnot.y - knot.y) > 1)
             {
-                tail.y += head.y > tail.y ? 1 : -1;
-                tail.x += head.x != tail.x ? (head.x > tail.x ? 1 : -1) : 0;
+                y += previousKnot.y > knot.y ? 1 : -1;
+                x += previousKnot.x != knot.x ? (previousKnot.x > knot.x ? 1 : -1) : 0;
+                return (x,y);
             }
+
+            return (x,y);
         }
-
-
     }
 }
