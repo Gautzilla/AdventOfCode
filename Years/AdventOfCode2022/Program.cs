fbf5b020f1df8c9948c5281a4ead7052f97eae46
Gautzilla <gauthier.berthomieu@gmail.com>
2022-12-20 18:44:22 +0100

    Add day 14

--- Years/AdventOfCode2022/Program.cs
+++ Years/AdventOfCode2022/Program.cs
@@ -16,7 +16,7 @@ namespace AdventOfCode2022
             if (int.TryParse(Console.ReadLine(), out int part) && part == 1 || part == 2) 
             {
                 stopWatch.Start();
-                Day13.Solve(part);
+                Day14.Solve(part);
                 stopWatch.Stop();
             }
             else
