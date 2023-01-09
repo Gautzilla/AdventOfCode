6e45c91a663add4983803b3bca42a3bb68cf9539
Gautzilla <gauthier.berthomieu@gmail.com>
2023-01-09 23:27:40 +0100

    Add comments for where to check for pattern

--- Years/AdventOfCode2022/Day17/Day17.cs
+++ Years/AdventOfCode2022/Day17/Day17.cs
@@ -20,17 +20,28 @@ namespace AdventOfCode2022
         private static HashSet<(int x, int y)> _occupied = new();
         private static readonly string _jetPattern = File.ReadAllText(@"Day17\input.txt");
         private static int _jetState = 0; 
+
+        private static int _deltaY = 0;
+
+        private static List<(int jetState, int deltaY)> _stoppedRocks = new(); // For checking repetitions
+        private static List<int> pattern = new();
         public static void Solve(int part)
         { 
             (int x, int y) startingPos = (3, -4);
+
+            long numberOfRocks = part == 1 ? 2022 : 1000000000000;
             
-            for (long rock = 0; rock < 2022; rock++)
+            for (long rock = 0; rock < numberOfRocks; rock++)
             {
                 if (_occupied.Count > 0) startingPos.y = _occupied.Min(o => o.y) - 4;
 
                 (int x, int y)[] fallingRock = _rocks[rock%_rocks.Length].Select(p => (p.x + startingPos.x, p.y + startingPos.y)).ToArray();
 
-                Fall(fallingRock);
+                if (pattern.Count == 0) Fall(fallingRock);
+                else 
+                {
+                    // Add pattern deltaY
+                }
             }
 
             Console.WriteLine(Math.Abs(_occupied.Min(o => o.y)));
@@ -47,15 +58,27 @@ namespace AdventOfCode2022
             // rock falls down
             if (rock.Any(p => p.y == -1) || rock.Any(p => _occupied.Contains((p.x, p.y+1))))
             {
-                _occupied = _occupied.Concat(rock).ToHashSet();
-                if(_jetState == 0) Console.WriteLine(_occupied.Min(p => p.y));
-                //Display(rock);
+                StopRock(rock);
                 return;
             }
 
             rock = rock.Select(p => (p.x, p.y + 1)).ToArray();
 
             Fall(rock);
+        }
+
+        private static void StopRock((int x, int y)[] rock)
+        {
+            _occupied = _occupied.Concat(rock).ToHashSet();
+
+            // Add height increase and jet state index for pattern finding
+            _stoppedRocks.Add((_jetState, _deltaY - _occupied.Min(o => o.y)));
+            _deltaY = _occupied.Min(o => o.y);
+            
+
+            // Check for pattern
+            
+            
         }
 
         private static void Display((int x, int y)[] falling)
