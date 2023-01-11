9061963df80b0b03ccfac06c88dc5318a92bc30b
Gautzilla <gauthier.berthomieu@gmail.com>
2023-01-11 16:00:23 +0100

    Add ugly part 2

--- Years/AdventOfCode2022/Day17/Day17.cs
+++ Years/AdventOfCode2022/Day17/Day17.cs
@@ -9,6 +9,28 @@ namespace AdventOfCode2022
 {
     public static class Day17
     {
+        class State
+        {
+            public int Jet { get; set; }
+            public int RockShape { get; set; }
+            public string Rocks { get; set; }
+            public int HeightGain { get; set; }
+
+            public State(int jet, int rockShape, HashSet<(int x, int y)> rocks)
+            {
+                Jet = jet;
+                RockShape = rockShape;
+                if (rocks.Count == 0) {Rocks = ""; return;}
+                int stateHeight = rocks.Max(r => r.y) - rocks.Min(r => r.y) +1;
+                Rocks = String.Join("\n", Enumerable.Range(rocks.Min(r => r.y), stateHeight).Select(y => String.Join("",Enumerable.Range(1, 7).Select(x => rocks.Contains((x,y)) ? '#' : '.'))));
+            }
+
+            public bool IsSameState(State other)
+            {
+                return Jet == other.Jet && RockShape == other.RockShape && Rocks == other.Rocks;
+            }
+        }
+
         private static readonly (int x, int y)[][] _rocks = { // rocks origin is at the bottom left corner
             new (int, int)[] {(0,0), (1,0), (2,0), (3,0)}, // _
             new (int, int)[] {(1,0), (0,-1), (1,-1), (2,-1), (1,-2)}, // +
@@ -19,35 +41,58 @@ namespace AdventOfCode2022
 
         private static HashSet<(int x, int y)> _occupied = new();
         private static readonly string _jetPattern = File.ReadAllText(@"Day17\input.txt");
-        private static int _height = 0;
+        private static long _height = 0;
         private static int _jetState = 0; 
-
-        private static int _deltaY = 0;
-
-        private static List<(int jetState, int deltaY)> _stoppedRocks = new(); // For checking repetitions
+        private static int _fallingRockIndex;
+        private static List<State> _states = new(); // Checks for repetition
         private static List<int> pattern = new();
         public static void Solve(int part)
         { 
             (int x, int y) startingPos = (3, -4);
 
             long numberOfRocks = part == 1 ? 2022 : 1000000000000;
+            long indexOfPattern = 0;
+
+            int nLines = 100; //Number of lines in buffer
             
             for (long rock = 0; rock < numberOfRocks; rock++)
             {
                 if (_occupied.Count > 0) startingPos.y = _occupied.Min(o => o.y) - 4;
 
                 (int x, int y)[] fallingRock = _rocks[rock%_rocks.Length].Select(p => (p.x + startingPos.x, p.y + startingPos.y)).ToArray();
+               
+                _fallingRockIndex = (int)(rock % _rocks.Length);
 
-                if (pattern.Count == 0) Fall(fallingRock);
-                else 
+                // Check for pattern
+                State currentState = new (_jetState, _fallingRockIndex, _occupied);
+                if (pattern.Count == 0 && _states.Any(s => s.IsSameState(currentState)))
                 {
-                    // Add pattern deltaY
+                    Console.WriteLine($"Same state: {rock}");
+                    State firstOfPattern = _states.Single(s => s.IsSameState(currentState));
+                    pattern = _states.Skip(_states.IndexOf(firstOfPattern)).Select(s => s.HeightGain).ToList();
+                    indexOfPattern = rock;
+                    _height = -(_height - nLines); // 
                 }
 
-                RemoveBottom(10);
+                if (pattern.Count == 0) 
+                {
+                    _states.Add(currentState);
+                    int prevHeight = _occupied.Count > 0 ? _occupied.Min(o => o.y) : 0;
+                    Fall(fallingRock);
+                    currentState.HeightGain = prevHeight - _occupied.Min(o => o.y);
+                    RemoveBottom(nLines);
+                }
+                else 
+                {
+                    _height += (numberOfRocks - rock)/pattern.Count()*pattern.Sum() + pattern.Take((int)((numberOfRocks - rock)%pattern.Count())).Sum();
+                    break;
+                    _height += pattern[(int)((rock-indexOfPattern)%pattern.Count)];
+                }
+                
             }
-
-            Console.WriteLine(Math.Abs(_occupied.Min(o => o.y) + _height));
+            
+            if (pattern.Count == 0) Console.WriteLine(Math.Abs(_height + _occupied.Min(o => o.y)));
+            else Console.WriteLine(_height);
         }
 
         private static void Fall((int x, int y)[] rock)
@@ -61,7 +106,7 @@ namespace AdventOfCode2022
             // rock falls down
             if (rock.Any(p => p.y == -1) || rock.Any(p => _occupied.Contains((p.x, p.y+1))))
             {
-                StopRock(rock);
+                _occupied = _occupied.Concat(rock).ToHashSet();
                 //Display(rock);
                 return;
             }
@@ -69,20 +114,6 @@ namespace AdventOfCode2022
             rock = rock.Select(p => (p.x, p.y + 1)).ToArray();
 
             Fall(rock);
-        }
-
-        private static void StopRock((int x, int y)[] rock)
-        {
-            _occupied = _occupied.Concat(rock).ToHashSet();
-
-            // Add height increase and jet state index for pattern finding
-            _stoppedRocks.Add((_jetState, _deltaY - _occupied.Min(o => o.y)));
-            _deltaY = _occupied.Min(o => o.y);
-            
-
-            // Check for pattern
-            
-            
         }
 
         private static void RemoveBottom(int nLines)
