a9d6067b6252b78d0ed59d247ec78e4fb333dd7c
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-01 10:16:48 +0100

    Add 2023

---
+++ Years/AdventOfCode2023/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using AdventOfCode2023;
+
+Stopwatch stopWatch = new Stopwatch();
+            Console.WriteLine("Part:");
+            if (int.TryParse(Console.ReadLine(), out int part) && part == 1 || part == 2) 
+            {
+                stopWatch.Start();
+                Day01.Solve(part);
+                stopWatch.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Part should be 1 or 2.");
+                return;
+            }
+
+            Console.WriteLine($"\nSolved in {stopWatch.ElapsedMilliseconds} ms");
