b38d9b2f3ee81fb23978796667f008d1d97bb5b4
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-03 14:15:02 +0100

    Add part 2

--- Years/AdventOfCode2023/Day03/Day03.cs
+++ Years/AdventOfCode2023/Day03/Day03.cs
@@ -5,6 +5,7 @@ namespace AdventOfCode2023
         private static char[][] _schematic = [];
         private static readonly (int x, int y)[] _directions = {(-1,0), (-1, -1), (0, -1), (1,-1), (1,0), (1,1), (0,1), (-1,1)};
         private static HashSet<Number> _numbers = new();
+        private static HashSet<Gear> _gears = new();
 
         private record Number
         {
@@ -28,9 +29,24 @@ namespace AdventOfCode2023
                         if(charToCheck == '.' || char.IsDigit(charToCheck)) continue;
                         
                         IsPart = true;
+
+                        if (charToCheck == '*') AddNumberToGear(this, (x + dir.x, Coordinates.y + dir.y));
+
                         return;
                     }
                 }
+            }
+        }
+
+        private record Gear
+        {
+            public (int x, int y) Coordinates { get; }
+            public List<Number> Numbers { get; set; } = new();
+            public int Ratio { get => Numbers.Aggregate(1, (n1, n2) => n1 * n2.Value); }
+
+            public Gear((int x, int y) coordinates)
+            {
+                Coordinates = coordinates;
             }
         }
 
@@ -42,6 +58,7 @@ namespace AdventOfCode2023
             ParseNumbers(input);
 
             if (part == 1) Console.WriteLine(_numbers.Where(n => n.IsPart).Sum(n => n.Value));
+            else Console.WriteLine(_gears.Where(g => g.Numbers.Count == 2).Sum(g => g.Ratio));
         }
 
         private static char[][] ParseSchematic(string[] input) => input.Select(s => s.ToCharArray()).ToArray();
@@ -66,6 +83,12 @@ namespace AdventOfCode2023
                     num = string.Empty;
                 }  
             }
+        }
+        private static void AddNumberToGear(Number number, (int x, int y) engineCoordinates)
+        {
+            Gear gear = _gears.FirstOrDefault(g => g.Coordinates == engineCoordinates) ?? new(engineCoordinates);
+            gear.Numbers.Add(number);
+            _gears.Add(gear);
         }
 
         private static bool IsValid(int x, int y) => x >= 0 && y >= 0 && x < _schematic.First().Length && y < _schematic.Length;
