65acfca1d2314a65086cf588ab376ec60e28a995
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-06 21:46:50 +0100

    Add part 2

--- Years/AdventOfCode2023/Day06/Day06.cs
+++ Years/AdventOfCode2023/Day06/Day06.cs
@@ -9,12 +9,17 @@ namespace AdventOfCode2023
 {
     public static class Day06
     {
-        private static (int time, int distance)[] _races = [];
         public static void Solve(int part)
         { 
             string[] input = File.ReadAllLines(@"Day06\input.txt");
 
-            _races = input
+            if (part == 1) SolvePart1(input);
+            else SolvePart2(input);
+        }
+
+        private static void SolvePart1(string[] input)
+        {
+            (int time, int distance)[] races = input
                 .First()
                 .ParseInts()
                 .Zip(input
@@ -22,12 +27,20 @@ namespace AdventOfCode2023
                     .ParseInts())
                 .ToArray();
 
-            Console.WriteLine(_races
-                .Aggregate(1, (a,b) => a * Enumerable.Range(1, b.time).Where(t => (b.time - t) * t > b.distance).Count()));
+            Console.WriteLine(races
+                .Aggregate(1, (a,b) => a * NbWinPossibilities(b.time, b.distance)));
+        }
+
+        private static void SolvePart2(string[] input)
+        {
+            (long time, long distance) = (long.Parse(string.Join("",input.First().Where(char.IsDigit))), long.Parse(string.Join("",input.Last().Where(char.IsDigit))));
+            Console.WriteLine(NbWinPossibilities(time, distance));        
         }
 
         private static IEnumerable<int> ParseInts (this string s) => s.Split(' ',StringSplitOptions.RemoveEmptyEntries)
                 .Where(s => int.TryParse(s, out int temp))
                 .Select(int.Parse);
+        private static long Distance(long pressTime, long totalTime) => (totalTime - pressTime) * pressTime;
+        private static int NbWinPossibilities(long time, long distance) => Enumerable.Range(1, (int)time).Where(t => Distance(t, time) > distance).Count();
     }
 }
