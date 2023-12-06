439a81cc27e385c20cfb58b068fab10a05fda799
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-06 15:21:44 +0100

    Add part 2

--- Years/AdventOfCode2023/Day05/Day05.cs
+++ Years/AdventOfCode2023/Day05/Day05.cs
@@ -9,84 +9,76 @@ namespace AdventOfCode2023
 {
     public static class Day05
     {
-        enum Category
-        {
-            Seed,
-            Soil,
-            Fertilizer,
-            Water,
-            Light,
-            Temperature,
-            Humidity,
-            Location
-        }
-
-        private record MapItem
-        {
-            public Category SourceCategory { get; set; }
-            public (long start, long stop) SourceRange { get; set; }
-            public long Offset { get; set; }
-
-            public MapItem(Category category, string mapLine)
-            {
-                SourceCategory = category;
-                long[] values = mapLine
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(long.Parse)
-                    .ToArray();
-
-                SourceRange = (values[1], values[1] + values[2] - 1);
-                Offset = values[0] - values[1];
-            }
-        }
-        private static List<(Category category, long value)> _seeds = new();
-        private static HashSet<MapItem> _map = new();
+        private static List<(long start, long stop, bool hasEvolved)> _seeds = new();
         public static void Solve(int part)
         { 
             string[] input = File.ReadAllLines(@"Day05\input.txt");
 
-            ParseSeeds(input.First());
-            ParseMaps(input.Skip(3));   
+            if (part == 1) ParseSeedsP1(input.First());   
+            else ParseSeedsP2(input.First());
 
-            if (part == 1) Console.WriteLine(_seeds.Select(s => PlantSeed([], s).seed.value).Min()); 
-
-        }
-
-        private static void ParseSeeds (string inputLine) => _seeds = inputLine
-                .Split(' ')
-                .Where(s => long.TryParse(s, out long i))
-                .Select(s => (Category.Seed,long.Parse(s)))
-                .ToList();
-
-        private static void ParseMaps(IEnumerable<string> input)
-        {
-            Category mapCategory = Category.Seed;
-            foreach (var line in input)
+            foreach (var line in input.Skip(1))
             {
-                if (line.Contains("map")) 
+                if (line == string.Empty) continue;
+
+                if (line.Contains("map"))
                 {
-                    mapCategory++;
+                    _seeds = _seeds.Select(s => (s.start, s.stop, false)).ToList();
                     continue;
                 }
-                if (line != string.Empty) _map.Add(new MapItem(mapCategory, line));
+
+                var mapItem = ParseMapItem(line);
+                List<(long start, long stop, bool hasEvolved)> newSeeds = new(_seeds.Where(s => s.hasEvolved));
+
+                foreach (var seed in _seeds.Where(s => !s.hasEvolved))
+                {
+                    if (!seed.Intercepts((mapItem.start, mapItem.stop)))
+                    {
+                         newSeeds.Add(seed);
+                         continue;
+                    }
+                    newSeeds.AddRange(Intersection(seed, (mapItem.start, mapItem.stop), mapItem.offset));
+                }
+
+                _seeds = new(newSeeds);
             }
+
+            Console.WriteLine(_seeds.Min(s => s.start));
         }
 
-        private static (List<(Category category, long value)> history,(Category category, long value) seed) PlantSeed(List<(Category category, long value)> history, (Category category, long value) seed)
+        private static void ParseSeedsP1 (string inputLine) => _seeds = inputLine
+                .Split(' ')
+                .Where(s => long.TryParse(s, out long i))
+                .Select(long.Parse)
+                .Select(i => (i,i, false))
+                .ToList();
+        
+        private static void ParseSeedsP2 (string inputLine) => _seeds = inputLine
+                .Split(' ')
+                .Where(s => long.TryParse(s, out long temp))
+                .Chunk(2)
+                .Select(c => (long.Parse(c.First()), long.Parse(c.First()) + long.Parse(c.Last()) - 1, false))
+                .ToList();
+
+        private static (long start, long stop, long offset) ParseMapItem(string line)
         {
-            history.Add(seed);
+            var ints = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            return (ints[1], ints[1] + ints[2], ints[0] - ints[1]);
+        }
 
-            if (seed.category == Category.Location) return (history, seed);
+        private static bool Intercepts(this (long start, long stop, bool hasEvolved) source, (long start, long stop) mapItem) => !(source.stop < mapItem.start || source.start > mapItem.stop);
 
-            var map = _map
-                .Where(m => m.SourceCategory == seed.category)
-                .FirstOrDefault(m => m.SourceRange.start <= seed.value && m.SourceRange.stop >= seed.value);
+        private static List<(long start, long stop, bool hasEvolved)> Intersection((long start, long stop, bool hasEvolved) source, (long start, long stop) mapItem, long mapItemOffset)
+        {
+            List<(long start, long stop, bool hasEvolved)> output = new();
 
-            seed.category++;
+            if (source.start < mapItem.start) output.Add((source.start, mapItem.start-1, false));
 
-            if(map != null) seed.value += map.Offset;
-            
-            return PlantSeed(history,(seed.category, seed.value));
+            output.Add((Math.Max(source.start, mapItem.start) + mapItemOffset, Math.Min(source.stop, mapItem.stop) + mapItemOffset, true));
+
+            if (source.stop > mapItem.stop) output.Add((mapItem.stop + 1, source.stop, false));
+
+            return output;
         }
     }
 }
