286110cbffe19ce9989ccd344567b0added8b5cc
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-11 15:31:09 +0100

    Add part 2

--- Years/AdventOfCode2023/Day11/Day11.cs
+++ Years/AdventOfCode2023/Day11/Day11.cs
@@ -1,19 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Diagnostics;
+using System.Numerics;
 
 namespace AdventOfCode2023
 {
     public static class Day11
     {
+        private static List<int> _expandedRowIndexes = [];
+        private static List<int> _expandedColumnIndexes = [];
         public static void Solve(int part)
         { 
             string[] input = File.ReadAllLines(@"Day11\input.txt");
 
-            input = input.Expand();
+            _expandedRowIndexes = Enumerable.Range(0, input.Length).Where(y => input[y].All(c => c!='#')).ToList();
+            _expandedColumnIndexes = Enumerable.Range(0, input.First().Length).Where(x => input.All(i => i[x] != '#')).ToList();
 
             List<(int x, int y)> galaxies = input
                 .Select((content, y) => (content, y))
@@ -23,40 +21,26 @@ namespace AdventOfCode2023
                 .Select(point => (point.x, point.y))
                 .ToList();
 
-            int totalDistance = 0;
+            BigInteger totalDistance = 0;
             for (int galaxy = 0; galaxy < galaxies.Count; galaxy++)
             {
                 for (int galaxy2 = galaxy+1; galaxy2 < galaxies.Count; galaxy2++)
                 {
-                    totalDistance += ManhattanDistance(galaxies[galaxy], galaxies[galaxy2]);
+                    totalDistance += ManhattanDistance(galaxies[galaxy], galaxies[galaxy2], part == 1 ? 1 : 1000000 - 1);
                 }
             }
 
             Console.WriteLine(totalDistance);
         }
 
-        private static string[] Expand (this string[] input)
-        {
-            var expandedRowIndexes = Enumerable.Range(0, input.Length).Where(y => input[y].All(c => c!='#'));
-            var expandedColumnIndexes = Enumerable.Range(0, input.First().Length).Where(x => input.All(i => i[x] != '#'));
-
-            List<string> temp = [];
-            for (int y = 0; y < input.First().Length; y++)
-            {
-                string inputLine = input[y];
-                string expandedLine = "";
-                for (int x = 0; x < inputLine.Length; x++)
-                {
-                    expandedLine += inputLine[x];
-                    if (expandedColumnIndexes.Contains(x)) expandedLine += inputLine[x];
-                }
-                temp.Add(expandedLine);
-                if (expandedRowIndexes.Contains(y)) temp.Add(expandedLine);
-            }
-
-            return [.. temp];
-        }
-
-        private static int ManhattanDistance ((int x, int y) c1, (int x, int y) c2) => Math.Abs(c2.x - c1.x) + Math.Abs(c2.y - c1.y);
+        private static int ManhattanDistance ((int x, int y) c1, (int x, int y) c2, int expansionRate) => 
+            Math.Abs(c2.x - c1.x) 
+            + _expandedColumnIndexes.Count(i => IsWithinColumns(i, c1, c2)) * expansionRate 
+            + Math.Abs(c2.y - c1.y) 
+            + _expandedRowIndexes.Count(i => IsWithinRows(i, c1, c2)) * expansionRate;
+        private static bool IsWithinColumns (int columnIndex, (int x, int y) c1, (int x, int y) c2) => 
+            columnIndex > Math.Min(c1.x, c2.x) && columnIndex < Math.Max(c1.x, c2.x);
+        private static bool IsWithinRows (int rowIndex, (int x, int y) c1, (int x, int y) c2) => 
+            rowIndex > Math.Min(c1.y, c2.y) && rowIndex < Math.Max(c1.y, c2.y);
     }
 }
