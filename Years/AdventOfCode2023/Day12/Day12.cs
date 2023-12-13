96b774907d0278ddcd445243e11818dc17502458
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-13 11:16:02 +0100

    Add part 2

--- Years/AdventOfCode2023/Day12/Day12.cs
+++ Years/AdventOfCode2023/Day12/Day12.cs
@@ -1,69 +1,63 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Diagnostics;
-using System.Text.RegularExpressions;
-
 namespace AdventOfCode2023
 {
     public static class Day12
     {
         public static void Solve(int part)
         { 
-            string[] input = File.ReadAllLines(@"Day12\input.txt");
+            string[] input = File.ReadAllLines(@"Day12\input.txt");    
 
-            int result = input.Sum(line => NbMatchingSpringSequences(line.GetSpringSequence(), line.GetRequiredConsecutiveLitSprings()));
+            input = input.Select(line => line.RepeatInput(part == 1 ? 1 : 5)).ToArray();
+
+            long result = input.Sum(line => NbMatchingSpringSequences(line.GetSpringSequence(), line.GetRequiredConsecutiveLitSprings()));
 
             Console.WriteLine(result);
         }
 
-        private static int[] GetRequiredConsecutiveLitSprings (this string line) => line
+        private static string RepeatInput (this string line, int numberOfTimes)
+        {
+            string springs = line.Split(' ').First(), requiredConsecutiveSprings = line.Split(' ').Last();
+            return string.Join("?", Enumerable.Range(0,numberOfTimes).Select(i => springs)) + " " + string.Join(",", Enumerable.Range(0,numberOfTimes).Select(i => requiredConsecutiveSprings));
+        }
+
+        private static List<int> GetRequiredConsecutiveLitSprings (this string line) => line
             .Split(' ')
             .Last()
             .Split(',')
             .Select(int.Parse)
-            .ToArray();
+            .ToList();
 
         private static string GetSpringSequence (this string line) => line
             .Split(' ')
             .First();
+            
+
+        private static Dictionary<string, long> _mem = [];
         
-        private static int NbMatchingSpringSequences (string springs, int[] litLengths)
+        private static long NbMatchingSpringSequences (string springs, List<int> litLengths)
         {
+            long result = 0;
 
-            int litLengthIndex = 0;
-            int nbConsecutiveLit = 0;
+            if (springs == "") return litLengths.Count == 0 ? 1 : 0;
 
-            foreach (var spring in springs)
+            if (litLengths.Count == 0) return springs.Any(c => c == '#') ? 0 : 1;
+
+            string key = springs + "_" + string.Join(",", litLengths);
+
+            if (_mem.TryGetValue(key, out long res)) return res;
+
+            if (".?".Contains(springs.First())) result += NbMatchingSpringSequences(springs[1..], new(litLengths));
+
+            if ("#?".Contains(springs.First()))
             {
-                if (spring == '?') return NbMatchingSpringSequences(springs.ReplaceUnknown(true), litLengths) + NbMatchingSpringSequences(springs.ReplaceUnknown(false), litLengths);
+                if (springs.Length < litLengths.First()) return result;
+                if (springs[..litLengths.First()].Contains('.')) return result;
 
-                if (spring == '.')
-                {
-                    if (nbConsecutiveLit == 0) continue;
-                    if (litLengthIndex < litLengths.Length && litLengths[litLengthIndex] != nbConsecutiveLit) return 0;
-                    litLengthIndex++;
-                    nbConsecutiveLit = 0;
-                }
-
-                if (spring == '#')
-                {
-                    if (litLengthIndex < litLengths.Length && ++nbConsecutiveLit > litLengths[litLengthIndex]) return 0;
-                }
+                if (springs.Length == litLengths.First() || springs[litLengths.First()] != '#') result += NbMatchingSpringSequences(springs[Math.Min(springs.Length, litLengths.First()+1)..], new(litLengths[1..]));
             }
 
-            return springs.Split('.', StringSplitOptions.RemoveEmptyEntries).Select(s => s.Length).SequenceEqual(litLengths) ? 1 : 0;
-        }
+            _mem.Add(key, result);
 
-        private static string ReplaceUnknown (this string springs, bool isNextSpringLit)
-        {
-            int index = springs.IndexOf('?');
-            var springsCharArray = springs.ToCharArray();
-            springsCharArray[index] = isNextSpringLit ? '#' : '.';
-
-            return new string(springsCharArray);
+            return result;
         }
     }
 }
