c54580b087134d30cae776191980cd473717dbf5
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-14 01:41:19 +0100

    Add part 1

--- Years/AdventOfCode2023/Program.cs
+++ Years/AdventOfCode2023/Program.cs
@@ -6,7 +6,7 @@ Stopwatch stopWatch = new Stopwatch();
             if (int.TryParse(Console.ReadLine(), out int part) && part == 1 || part == 2) 
             {
                 stopWatch.Start();
-                Day12.Solve(part);
+                Day13.Solve(part);
                 stopWatch.Stop();
             }
             else
