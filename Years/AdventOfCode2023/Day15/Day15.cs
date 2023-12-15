ce8625c67a6ff1b52f5683896cb6e54cb4a5cc37
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-15 13:13:56 +0100

    Add part 2

--- Years/AdventOfCode2023/Day15/Day15.cs
+++ Years/AdventOfCode2023/Day15/Day15.cs
@@ -5,25 +5,73 @@ using System.IO;
 using System.Text;
 using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023
 {
     public static class Day15
     {
+
+        private const int _nbBoxes = 256;
+        private static List<(string label, int focalLength)>[] _boxes = [];
         public static void Solve(int part)
         { 
-            string[] input = File.ReadAllText(@"Day15\input.txt").Split(',');
+            string[] input = File.ReadAllText(@"C:\Users\Gauthier\source\repos\AdventOfCode\Years\AdventOfCode2023\Day15\input.txt").Split(',');
 
-            Console.WriteLine(input.Sum(HASH));
+            if (part == 1) Console.WriteLine(input.Sum(HASH));
+            else
+            {
+                _boxes = Enumerable.Range(0, _nbBoxes).Select(i => new List<(string label, int focalLength)>()).ToArray();
+                foreach (var instruction in input) ProcessInstruction(instruction);
+                Console.WriteLine(_boxes.HASHMAP());
+            }
+        }
+        
+        private static int HASH (this string s) => s.Aggregate(0, (a, b) => ((a + b) * 17) % 256);
+
+        private static void ProcessInstruction(string instruction)
+        {
+            string regex = @"(?<label>\w+)(?<operation>[=-])(?<focalLength>\d+)?";
+            Match match = Regex.Match(instruction, regex);
+
+            string label = match.Groups["label"].Value;            
+            int boxIndex = label.HASH();
+            string operation = match.Groups["operation"].Value;
+
+            if (operation == "=")
+            {
+                int focalLength = int.Parse(match.Groups["focalLength"].Value);
+                AddLense(boxIndex, label, focalLength);
+            } else
+            {
+                RemoveLense(boxIndex, label);
+            }
         }
 
-        /*
-            Determine the ASCII code for the current character of the string.
-            Increase the current value by the ASCII code you just determined.
-            Set the current value to itself multiplied by 17.
-            Set the current value to the remainder of dividing itself by 256.
-        */
+        private static void RemoveLense (int boxIndex, string label)
+        {
+            _boxes[boxIndex] = _boxes[boxIndex].Where(lense => lense.label != label).ToList();
+        }
 
-        private static int HASH (this string s) => s.Aggregate(0, (a, b) => ((a + b) * 17) % 256);
+        private static void AddLense (int boxIndex, string label, int focalLength)
+        {
+            var box = _boxes[boxIndex];
+            
+            if (!box.Any(lense => lense.label == label))
+            {
+                box.Add((label, focalLength));
+                return;
+            }
+            
+            int indexLenseToReplace = box.IndexOf(box.First(lense => lense.label == label));
+            box[indexLenseToReplace] = (label, focalLength);
+        }
+
+        private static int HASHMAP (this List<(string label, int focalLength)>[] boxes) => boxes
+            .Select((content, boxIndex) => (boxIndex + 1) * content
+                .Select((lense, slotINdex) => (slotINdex + 1) * lense.focalLength)
+                .Sum())
+            .Sum();
     }
 }
