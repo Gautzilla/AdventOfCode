371c22ecda83a1c4059c543afb3d717e7957fef1
Gautzilla <gauthier.berthomieu@gmail.com>
2023-12-15 02:17:08 +0100

    Improve 2023 day 14 part 2

--- Years/AdventOfCode2023/Day14/Day14.cs
+++ Years/AdventOfCode2023/Day14/Day14.cs
@@ -6,26 +6,60 @@ namespace AdventOfCode2023
         { 
             List<string> input = [.. File.ReadAllLines(@"Day14\input.txt")];
 
-            input = input.Rotate(true)
-                .Select(RollLeft).ToList()
-                .Rotate(false);
+            int score = 0;
+
+            if (part == 1) score = input
+                .Rotate(false)
+                .Roll(true)
+                .Rotate(true)
+                .Score();
+
+            if (part == 2)
+            {
+                List<string> memory = [];
+                List<int> scores = [];
+
+                int loopStartIndex = -1;
+
+                while (loopStartIndex == -1)
+                {
+                    memory.Add(input.Store());
+                    scores.Add(input.Score());                    
+
+                    input = input
+                        .Rotate(false)
+                        .Roll(true)
+                        .Rotate(true)
+                        .Roll(true)
+                        .Rotate(true)
+                        .Roll(true)
+                        .Rotate(false)
+                        .Roll(false);
+
+                    loopStartIndex = memory.IndexOf(input.Store());                                      
+                }
+
+                score = scores[ResultIndex(1000000000, memory.Count, loopStartIndex)];                
+            }
             
-            Console.WriteLine(Score(input));
+            Console.WriteLine(score);
         }
         
-        private static int Score (List<string> input) => input.Select((row, y) => row.Count(c => c == 'O') * (input.Count - y)).Sum();
+        private static int Score (this List<string> rocks) => rocks.Select((row, y) => row.Count(c => c == 'O') * (rocks.Count - y)).Sum();
 
-        private static List<string> Rotate (this List<string> input, bool isClockwise)
+        private static string Store (this List<string> rocks) => string.Join("\r\n", rocks);
+
+        private static List<string> Rotate (this List<string> rocks, bool isClockwise)
         {
             List<string> result = [];
 
-            for (int y = 0; y < input.First().Length; y++)
+            for (int y = 0; y < rocks.First().Length; y++)
             {
                 string newLine = "";
-                for (int x = 0; x < input.Count; x++)
+                for (int x = 0; x < rocks.Count; x++)
                 {
-                    if (isClockwise) newLine += input[x][input.First().Length - y - 1];
-                    else newLine += input[input.Count - x -1][y];
+                    if (!isClockwise) newLine += rocks[x][rocks.First().Length - y - 1];
+                    else newLine += rocks[rocks.Count - x -1][y];
                 }
                 result.Add(newLine);
             }
@@ -33,9 +67,14 @@ namespace AdventOfCode2023
             return result;
         }
 
-        private static string RollLeft (string line) => string.Join("#", 
-            line.Split('#')
-                .Select(roundedRocks => string.Join("", roundedRocks
-                        .OrderBy(c => "O.".IndexOf(c)))));
+        private static List<string> Roll (this List<string> rocks, bool isRollLeft) => rocks
+            .Select(line => string.Join("#", 
+                line.Split('#')
+                    .Select(roundedRocks => string.Join("", roundedRocks
+                            .OrderBy(c => (isRollLeft ? "O." : ".O").IndexOf(c))))))
+            .ToList();
+
+        private static int ResultIndex(int nbTotalCycles, int memoryCount, int loopStartIndex) => 
+            (nbTotalCycles - loopStartIndex )%(memoryCount - loopStartIndex ) + loopStartIndex;
     }
 }
