8f31600611c285e01368468f5e0791f090e07370
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-01 20:15:36 +0100

    add 2015 day 3

--- python_AoC/2015/03/2015_03.py
+++ python_AoC/2015/03/2015_03.py
@@ -1,11 +1,30 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
-def part1(puzzle_input: str) -> any:
-	pass
+directions = {
+	">": (1,0),
+	"^": (0,1),
+	"v": (0,-1),
+	"<": (-1,0),
+}
 
-def part2(puzzle_input: str) -> any:
-	pass
+def deliver(instructions: str, position: tuple[int, int], visited_houses: set) -> None:
+	visited_houses.add(position)
+	for c in instructions:
+		direction = directions[c]
+		position = (position[0] + direction[0], position[1] + direction[1])
+		visited_houses.add(position)
+
+def part1(puzzle_input: str) -> int:
+	visited_houses = set()
+	deliver(instructions=puzzle_input, position=(0,0), visited_houses=visited_houses)
+	return len(visited_houses)
+
+def part2(puzzle_input: str) -> int:
+	visited_houses = set()
+	deliver(instructions=puzzle_input[::2], position=(0, 0), visited_houses=visited_houses)
+	deliver(instructions=puzzle_input[1::2], position=(0, 0), visited_houses=visited_houses)
+	return len(visited_houses)
 
 def solve(puzzle_input: str) -> tuple[any, any]:
 	part_one = part1(puzzle_input)
@@ -14,7 +33,7 @@ def solve(puzzle_input: str) -> tuple[any, any]:
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
