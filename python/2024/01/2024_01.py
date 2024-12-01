fdadfb9a17a00dcc2bb5bc303644eb1edb7c44ec
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-01 13:45:43 +0100

    add 2024-01 part 1

--- python/2024/01/2024_01.py
+++ python/2024/01/2024_01.py
@@ -4,4 +4,11 @@ with open("example_input.txt") as e:
 with open("puzzle_input.txt") as i:
 	puzzle_input = i.read()
 
-items = example_input.split("\n")
+#i = example_input
+i = puzzle_input
+
+lines = [[int(v) for v in line.split(" ") if v] for line in i.split("\n")]
+list_one = sorted(line[0] for line in lines)
+list_two = sorted(line[1] for line in lines)
+comparison = zip(list_one, list_two)
+print(sum(abs(c[1] - c[0]) for c in comparison))
