00f6511da412b79e2005d81a8feb5e9693ac6d95
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-01 20:38:26 +0100

    add 2015_04

--- python_AoC/2015/04/2015_04.py
+++ python_AoC/2015/04/2015_04.py
@@ -1,11 +1,18 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
+import hashlib
+
+def find_hash_with_leading_zeroes(puzzle_input: str, nb_zeroes: int) -> int:
+	output = 0
+	while str(hashlib.md5(f"{puzzle_input}{output}".encode()).hexdigest())[:nb_zeroes] != "0" * nb_zeroes:
+		output += 1
+	return output
 
 def part1(puzzle_input: str) -> any:
-	return ""
+	return find_hash_with_leading_zeroes(puzzle_input=puzzle_input, nb_zeroes=5)
 
 def part2(puzzle_input: str) -> any:
-	return ""
+	return find_hash_with_leading_zeroes(puzzle_input=puzzle_input, nb_zeroes=6)
 
 def solve(puzzle_input: str) -> tuple[any, any]:
 	part_one = part1(puzzle_input)
@@ -14,7 +21,7 @@ def solve(puzzle_input: str) -> tuple[any, any]:
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
