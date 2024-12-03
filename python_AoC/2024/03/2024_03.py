6ad628102475627939154441997a6d8a0a745b12
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-03 13:47:01 +0100

    add 2024_03

--- python_AoC/2024/03/2024_03.py
+++ python_AoC/2024/03/2024_03.py
@@ -1,11 +1,25 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
+import re
 
-def part1(puzzle_input: str) -> any:
-	return ""
+PATTERN = r"mul\((?P<n1>\d{1,3}),(?P<n2>\d{1,3})\)"
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def part1(memory: str) -> any:
+	values = re.findall(PATTERN, memory)
+	return sum(int(a) * int(b) for a, b in values)
+
+def remove_disabled_parts(memory: str) -> str:
+	if "don't" not in memory:
+		return memory
+	dont_index = memory.index("don't")
+	do_index = list(i.regs[0][1] for i in re.finditer("do(?!n't)", memory))
+	do_index = [idx for idx in do_index if idx > dont_index]
+	do_index = len(memory) - 1 if not do_index else do_index[0]
+	return remove_disabled_parts(memory[:dont_index] + memory[do_index:])
+
+def part2(memory: str) -> any:
+	memory = remove_disabled_parts(memory)
+	return part1(memory)
 
 def solve(puzzle_input: str) -> tuple[any, any]:
 	part_one = part1(puzzle_input)
@@ -14,7 +28,7 @@ def solve(puzzle_input: str) -> tuple[any, any]:
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
