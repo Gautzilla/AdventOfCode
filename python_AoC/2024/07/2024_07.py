f56e7025f1081aa5352717c235e4109d5028e8a8
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-08 19:18:22 +0100

    add 2024_07

--- python_AoC/2024/07/2024_07.py
+++ python_AoC/2024/07/2024_07.py
@@ -1,20 +1,36 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
-def part1(puzzle_input: str) -> any:
-	return ""
+def is_solvable(result: int, numbers: list[int], current: int, index: int, part: int):
+	if len(numbers) == 0:
+		return current == result
+	if current > result:
+		return False
+	if part == 1:
+		return is_solvable(result, numbers[1:], current+numbers[0], index+1,part) or is_solvable(result, numbers[1:], current*numbers[0], index+1,part)
+	return is_solvable(result, numbers[1:], current + numbers[0], index + 1,part) or is_solvable(result, numbers[1:],
+																							current * numbers[0],
+																							index + 1,part) or is_solvable(result, numbers[1:],
+																							int(str(current)+str(numbers[0])),
+																							index + 1,part)
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def part1(equations: list[tuple[int, list[int]]]) -> any:
+	return sum(equation[0] for equation in equations if is_solvable(equation[0], equation[1][1:], equation[1][0], 1,1))
+
+def part2(equations: list[tuple[int, list[int]]]) -> any:
+	return sum(equation[0] for equation in equations if is_solvable(equation[0], equation[1][1:], equation[1][0], 1,2))
 
 def solve(puzzle_input: str) -> tuple[any, any]:
-	part_one = part1(puzzle_input)
-	part_two = part2(puzzle_input)
+	results = [int(line.split(":")[0]) for line in puzzle_input.splitlines()]
+	numbers = [[int(v) for v in line.split(":")[1].split()] for line in puzzle_input.splitlines()]
+	equations = list(zip(results, numbers))
+	part_one = part1(equations)
+	part_two = part2(equations)
 
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
