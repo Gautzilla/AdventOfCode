d4b4a32e74a60dc6a1a980e2a7fa18b683e5edf9
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-08 18:53:14 +0100

    add 2024_06

--- python_AoC/2024/06/2024_06.py
+++ python_AoC/2024/06/2024_06.py
@@ -1,20 +1,60 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
-def part1(puzzle_input: str) -> any:
-	return ""
+DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
+DIRECTIONS_STR = ">v<^"
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def parse_grid(puzzle_input: str):
+	obstacles = set()
+	position = (0,0)
+	direction = 0
+	size = (len(puzzle_input.splitlines()[0]),len(puzzle_input.splitlines()))
+	for y, line in enumerate(puzzle_input.splitlines()):
+		for x, c in enumerate(line):
+			if c == "#":
+				obstacles.add((x,y))
+			if c in DIRECTIONS_STR:
+				position = (x,y)
+				direction = DIRECTIONS_STR.index(c)
+	return obstacles, position, direction, size
+
+def run_path(obstacles, position, direction, grid_size):
+	visited = set()
+
+	while True:
+		if (position, direction) in visited:
+			return set(coord for coord, direction in visited), 1
+		visited.add((position, direction))
+		next_pos = (position[0] + DIRECTIONS[direction][0], position[1] + DIRECTIONS[direction][1])
+		if not (0 <= next_pos[0] < grid_size[0] and 0 <= next_pos[1] < grid_size[1]):
+			return set(coord for coord, direction in visited), 0
+		if next_pos in obstacles:
+			direction = (direction + 1) % len(DIRECTIONS)
+		else:
+			position = next_pos
+
+def part1(obstacles, position, direction, grid_size) -> any:
+	return run_path(obstacles, position, direction, grid_size)[0]
+
+
+def part2(obstacles, position, direction, grid_size, guard_path) -> any:
+	nb_loops = 0
+	guard_path = set(p for p in guard_path if p != direction)
+	for coord in guard_path:
+		new_obstacles = obstacles.copy()
+		new_obstacles.add(coord)
+		nb_loops += run_path(new_obstacles, position, direction, grid_size)[1]
+	return nb_loops
 
 def solve(puzzle_input: str) -> tuple[any, any]:
-	part_one = part1(puzzle_input)
-	part_two = part2(puzzle_input)
+	obstacles, position, direction, grid_size = parse_grid(puzzle_input)
+	visited = part1(obstacles, position, direction, grid_size)
+	part_two = part2(obstacles, position, direction, grid_size, visited)
 
-	return part_one, part_two
+	return len(visited), part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
