24cafaf5b95c2e031931641bea758bad07cbf2d5
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-10 17:47:05 +0100

    add 2024_10

--- python_AoC/2024/10/2024_10.py
+++ python_AoC/2024/10/2024_10.py
@@ -1,20 +1,40 @@
+from debugpy.common.timestamp import current
+
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
-def part1(puzzle_input: str) -> any:
-	return ""
+def neighbors(grid,x,y,current_height):
+	directions = ((0,1), (1,0), (0,-1), (-1,0))
+	possible_neighbors = ((x+d[0], y+d[1]) for d in directions)
+	return [n for n in possible_neighbors if is_in_grid(grid,*n) and grid[n[1]][n[0]] == current_height+1]
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def is_in_grid(grid, x, y) -> bool:
+	return 0 <= y < len(grid) and 0 <= x < len(grid[0])
 
-def solve(puzzle_input: str) -> tuple[any, any]:
-	part_one = part1(puzzle_input)
-	part_two = part2(puzzle_input)
+def hike(grid, x, y, current_height, tops: set):
+	if current_height == 9:
+		tops.add((x, y))
+		return 1
+	reachable_neighbors = neighbors(grid,x,y,current_height)
+	return sum(hike(grid, xn, yn, current_height+1, tops) for xn,yn in reachable_neighbors)
 
+def solve_grid(grid: list[list[int]]) -> tuple[int,int]:
+	part_one = 0
+	part_two = 0
+	for y in range(len(grid)):
+		for x in range(len(grid[0])):
+			if grid[y][x] == 0:
+				tops = set()
+				part_two += hike(grid, x,y, 0,tops)
+				part_one += len(tops)
 	return part_one, part_two
 
+def solve(puzzle_input: str) -> tuple[any, any]:
+	grid = [[int(c) for c in line] for line in puzzle_input.splitlines()]
+	return solve_grid(grid)
+
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
