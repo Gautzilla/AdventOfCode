7e97981747ab47046cdbd427abb52d1e1bf7c84a
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-10 14:27:00 +0100

    add 2024_09

--- python_AoC/2024/09/2024_09.py
+++ python_AoC/2024/09/2024_09.py
@@ -2,10 +2,41 @@ from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
 def part1(puzzle_input: str) -> any:
-	return ""
+	file = []
+	empty_blocks = []
+	empty_block_idx = 0
+	for i,c in enumerate(puzzle_input):
+		if i % 2:
+			empty_blocks += list(range(empty_block_idx, empty_block_idx + int(c)))
+		else:
+			file += [i//2]*int(c)
+		empty_block_idx += int(c)
+	for i in empty_blocks:
+		if i >= len(file):
+			break
+		file.insert(i, file.pop())
+
+	return sum(int(c)*i for i,c in enumerate(file))
 
 def part2(puzzle_input: str) -> any:
-	return ""
+	files = []
+	empty = []
+	idx = 0
+	for i,c in enumerate(puzzle_input):
+		if i % 2:
+			empty.append([*range(idx, idx + int(c))])
+		else:
+			files.append([*range(idx, idx + int(c))])
+		idx += int(c)
+
+	for index, file in list(enumerate(files.copy()))[::-1]:
+		for empty_index in range(len(empty)):
+			if len(empty[empty_index]) >= len(file) and empty[empty_index][0] < file[0]:
+				files[index] = empty[empty_index][:len(file)]
+				empty[empty_index] = empty[empty_index][len(file):]
+				break
+
+	return sum(int(f) * i for i,file in enumerate(files) for f in file)
 
 def solve(puzzle_input: str) -> tuple[any, any]:
 	part_one = part1(puzzle_input)
@@ -14,7 +45,7 @@ def solve(puzzle_input: str) -> tuple[any, any]:
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
