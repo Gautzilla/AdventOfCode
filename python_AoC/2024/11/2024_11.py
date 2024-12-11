00790508af8a789eda0afeb4f7baaab3e7f25bf8
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-11 22:04:02 +0100

    add 2024_11

--- python_AoC/2024/11/2024_11.py
+++ python_AoC/2024/11/2024_11.py
@@ -1,20 +1,46 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
-def part1(puzzle_input: str) -> any:
-	return ""
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def add_stone(stones: dict, stone: int, count: int):
+	if stone in stones:
+		stones[stone] += count
+	else:
+		stones[stone] = count
+
+def blink_stone(stone: int) -> list[int]:
+	if stone == 0:
+		return [1]
+	if not len(s := str(stone)) % 2:
+		return [int(s[:len(s)//2]), int(s[len(s)//2:])]
+	return [2024*stone]
+
+def blink(stones: dict[int,int]) -> any:
+	next_stones = {}
+
+	for stone, count in stones.items():
+		for replacement in blink_stone(stone):
+			add_stone(next_stones, replacement, count)
+	return next_stones
+
+def solve_parts(stones: dict[int,int], nb_blinks: int) -> int:
+	for _ in range(nb_blinks):
+		stones = blink(stones)
+	return sum(stones.values())
 
 def solve(puzzle_input: str) -> tuple[any, any]:
-	part_one = part1(puzzle_input)
-	part_two = part2(puzzle_input)
+	starting_stones = [int(s) for s in puzzle_input.split(" ")]
+	stones = {}
+	for stone in starting_stones:
+		add_stone(stones, stone, 1)
+
+	part_one = solve_parts(stones, nb_blinks=25)
+	part_two = solve_parts(stones, nb_blinks=75)
 
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
