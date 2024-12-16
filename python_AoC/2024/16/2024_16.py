59a0c1234e06d5b013d47dd63284529cdfe747a2
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-16 18:15:56 +0100

    add slooooow 2024_16

--- python_AoC/2024/16/2024_16.py
+++ python_AoC/2024/16/2024_16.py
@@ -1,20 +1,61 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
 
-def part1(puzzle_input: str) -> any:
-	return ""
+DIRECTIONS = ((1,0), (0,1), (-1,0), (0,-1))
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def search(obstacles, run, runs, visited):
+	position, run_hist, direction, cost = run
+	visited.append((position, direction))
+	run_hist = list(set(run_hist+[position]))
+	for d in range(direction-1,direction+2):
+		dir = DIRECTIONS[d%len(DIRECTIONS)]
+		position2 = tuple(map(sum, zip(position, dir)))
+		if position2 in obstacles:
+			continue
+		if (position2, d) in visited or (position2,(d+2)%len(DIRECTIONS)) in visited:
+			continue
+		cost2 = cost + 1000 if d != direction else cost
+		runs.append((position2, run_hist, d, cost2+1))
+
+def solve_puzzle(obstacles, start, end) -> tuple:
+	runs = [(start, [start], 0, 0)]
+	visited = []
+	best_spots = {start}
+	best_path_cost = None
+	while len(runs) > 0:
+		runs = sorted(runs, key = lambda r: r[-1])
+		cheapest = runs.pop(0)
+		if best_path_cost is not None and cheapest[-1] > best_path_cost:
+			break
+		if cheapest[0] == end:
+			best_path_cost = cheapest[-1]
+			best_spots |= set(cheapest[1])
+		search(obstacles, cheapest, runs, visited)
+	return best_path_cost, len(best_spots)+1
+
+def parse_grid(puzzle_input: str) -> tuple[set[tuple[int,int]], tuple[int,int], tuple[int,int]]:
+	start = (0,0)
+	end = (0,0)
+	obstacles = set()
+	for y, line in enumerate(puzzle_input.splitlines()):
+		for x, c in enumerate(line):
+			if c == "S":
+				start = (x,y)
+			if c == "E":
+				end = (x,y)
+			if c == "#":
+				obstacles.add((x,y))
+	return obstacles, start, end
 
 def solve(puzzle_input: str) -> tuple[any, any]:
-	part_one = part1(puzzle_input)
-	part_two = part2(puzzle_input)
+	obstacles, start, end = parse_grid(puzzle_input)
+
+	part_one, part_two = solve_puzzle(obstacles, start, end)
 
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
