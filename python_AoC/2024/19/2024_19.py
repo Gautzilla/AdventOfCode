0f3fc364743ebffea42058645dc4af78f3e83f6f
Gauthier BERTHOMIEU <berthoga@ensieta.ecole>
2024-12-23 15:55:16 +0100

    add 2024_19 part 2

--- python_AoC/2024/19/2024_19.py
+++ python_AoC/2024/19/2024_19.py
@@ -7,29 +7,33 @@ cache = {}
 
 def search(towels, design):
 	if design == "":
-		return True
+		return 1
 	if design in cache:
 		return cache[design]
 	for towel in towels:
 		if design.startswith(towel) and search(towels, design[len(towel):]):
-			cache[design] = True
-			return True
-	cache[design] = False
-	return False
+			adds = search(towels, design[len(towel):])
+			if design in cache:
+				cache[design] += adds
+			else :
+				cache[design] = adds
+	if design not in cache:
+		cache[design] = 0
+	return 0 if design not in cache else cache[design]
 
 def part1(towels, designs) -> any:
 	return len([design for design in designs if search(list(towels), design)])
 
 
-def part2(puzzle_input: str) -> any:
-	return ""
+def part2(towels, designs) -> any:
+	return sum(search(list(towels), design) for design in designs)
 
 def solve(puzzle_input: str) -> tuple[any, any]:
 	inp = puzzle_input.splitlines()
 	towels = [t.strip() for t in inp[0].split(",")]
 	designs = inp[2:]
 	part_one = part1(towels, designs)
-	part_two = part2(puzzle_input)
+	part_two = part2(towels, designs)
 
 	return part_one, part_two
 
