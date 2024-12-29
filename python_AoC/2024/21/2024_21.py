6561dfa31002cc50cf83a9dd77c5bf188f3c482e
Gauthier BERTHOMIEU <berthoga@ensieta.ecole>
2024-12-29 11:17:17 +0100

    add 2024_21 part 2

--- python_AoC/2024/21/2024_21.py
+++ python_AoC/2024/21/2024_21.py
@@ -1,5 +1,6 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
+from functools import cache
 
 MAIN_KEYBOARD = {
 	"0": ["","^<","^","^>","^^<","^^","^^>","^^^<","^^^","^^^>",">"],
@@ -53,27 +54,24 @@ DIR_KEYBOARD = {
 	},
 }
 
-
+@cache
+def path_length(a,b,depth):
+	if depth == 0:
+		return len(DIR_KEYBOARD[a][b] + "A")
+	next_sequence = DIR_KEYBOARD[a][b]
+	return sum(path_length(*p, depth-1) for p in zip("A" + next_sequence, next_sequence + "A"))
 
 def drive_robots(puzzle_input: str, nb_robots: int) -> int:
 	output = {}
 	for input in puzzle_input.splitlines():
 		sequence = ""
-		sequence2 = ""
 		cc = "A"
 		for c in input:
 			i = int(c) if c.isdigit() else 10
 			sequence += MAIN_KEYBOARD[cc][i] + "A"
 			cc = c
-		for _ in range(nb_robots):
-			cc = "A"
-			for c in sequence:
-				sequence2 += DIR_KEYBOARD[cc][c] + "A"
-				cc = c
-			sequence = sequence2
-			sequence2 = ""
-		output[input] = sequence
-	return sum(int(key[:-1]) * len(value) for key,value in output.items())
+		output[input] = sum(path_length(*p, nb_robots-1) for p in zip("A" + sequence, sequence))
+	return sum(int(key[:-1]) * value for key,value in output.items())
 
 def part1(puzzle_input: str) -> any:
 	return drive_robots(puzzle_input, 2)
@@ -83,7 +81,7 @@ def part2(puzzle_input: str) -> any:
 
 def solve(puzzle_input: str) -> tuple[any, any]:
 	part_one = part1(puzzle_input)
-	part_two = "" # part2(puzzle_input)
+	part_two = part2(puzzle_input)
 
 	return part_one, part_two
 
