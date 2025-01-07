86447889406c9372fb2c2e373bb23866f8bcfe4c
Gautzilla <gauthier.berthomieu@gmail.com>
2025-01-07 13:21:24 +0100

    add 2024_23 part 1

--- python_AoC/2024/23/2024_23.py
+++ python_AoC/2024/23/2024_23.py
@@ -1,8 +1,23 @@
 from template_creator import read_input
 from pandas import Timestamp, Timedelta
+from itertools import combinations
 
 def part1(puzzle_input: str) -> any:
-	return ""
+	computers = {}
+	for line in puzzle_input.splitlines():
+		first, second = line.split("-")
+		for a,b in (first, second), (second, first):
+			if a not in computers:
+				computers[a] = [b]
+				continue
+			computers[a].append(b)
+	combs = set()
+	for computer, links in computers.items():
+		combs |= set(tuple(sorted(c)) for c in (combinations([computer] + links, 3)))
+
+	combs = (c for c in combs if any(s[0] == "t" for s in c))
+	combs = (c for c in combs if all(c2 in computers[c1] for c1 in c for c2 in (c3 for c3 in c if c3 != c1)))
+	return sum(1 for _ in combs)
 
 def part2(puzzle_input: str) -> any:
 	return ""
@@ -14,7 +29,7 @@ def solve(puzzle_input: str) -> tuple[any, any]:
 	return part_one, part_two
 
 if __name__ == "__main__":
-	i = read_input(example = True)
+	i = read_input(example = False)
 
 	t_start = Timestamp.now()
 	p1, p2 = solve(puzzle_input=i)
