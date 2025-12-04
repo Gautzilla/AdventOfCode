4bcc08eecfd312d64186e0e66d26fb7ca10b345f
Gautzilla <72027971+Gautzilla@users.noreply.github.com>
2025-12-04 15:47:22 +0100

    add 2025 day 4

--- python_AoC/2025/03/2025_03.py
+++ python_AoC/2025/03/2025_03.py
@@ -1,20 +1,28 @@
 from template_creator import run_puzzle
 
+
 def highest_joltage(batteries: list[int], nb_lit_batteries: int) -> int:
     lit_batteries = []
     for battery in range(nb_lit_batteries):
-        start_idx = 0 if not lit_batteries else (lit_batteries[-1][0]+1)
-        stop_idx = len(batteries) - (nb_lit_batteries-len(lit_batteries)) + 1
-        lit_batteries.append(max(list(enumerate(batteries))[start_idx:stop_idx], key=lambda b: b[1]))
+        start_idx = 0 if not lit_batteries else (lit_batteries[-1][0] + 1)
+        stop_idx = len(batteries) - (nb_lit_batteries - len(lit_batteries)) + 1
+        lit_batteries.append(
+            max(list(enumerate(batteries))[start_idx:stop_idx], key=lambda b: b[1])
+        )
     return int("".join(str(b[1]) for b in lit_batteries))
 
 
 def part1(puzzle_input: str) -> int:
-    return sum(highest_joltage([int(b) for b in line], 2) for line in puzzle_input.splitlines())
+    return sum(
+        highest_joltage([int(b) for b in line], 2) for line in puzzle_input.splitlines()
+    )
 
 
 def part2(puzzle_input: str) -> int:
-    return sum(highest_joltage([int(b) for b in line], 12) for line in puzzle_input.splitlines())
+    return sum(
+        highest_joltage([int(b) for b in line], 12)
+        for line in puzzle_input.splitlines()
+    )
 
 
 def solve(puzzle_input: str) -> tuple[int, int]:
--- python_AoC/2025/04/2025_04.py
+++ python_AoC/2025/04/2025_04.py
@@ -1,20 +1,44 @@
 from template_creator import run_puzzle
 
 
-def part1(puzzle_input: str) -> any:
-    return ""
+def neighbours(coords: tuple[int, int]) -> set[tuple[int, int]]:
+    directions = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
+    return set((coords[0] + x, coords[1] + y) for x, y in directions)
 
 
-def part2(puzzle_input: str) -> any:
-    return ""
+def nb_neighbours(coords: tuple[int, int], rolls_coords: set[tuple[int, int]]) -> int:
+    return sum(1 for neighbour in neighbours(coords) if neighbour in rolls_coords)
 
 
-def solve(puzzle_input: str) -> tuple[any, any]:
-    part_one = part1(puzzle_input)
-    part_two = part2(puzzle_input)
+def is_movable(coords: tuple[int, int], rolls_coords: set[tuple[int, int]]) -> bool:
+    return nb_neighbours(coords, rolls_coords) < 4
+
+
+def part1(rolls_coords: set[tuple[int, int]]) -> int:
+    return sum(1 for roll in rolls_coords if is_movable(roll, rolls_coords))
+
+
+def part2(rolls_coords: set[tuple[int, int]]) -> int:
+    removed_rolls = set()
+    while rolls_to_remove := set(
+        roll for roll in rolls_coords if is_movable(roll, rolls_coords)
+    ):
+        removed_rolls |= rolls_to_remove
+        rolls_coords -= rolls_to_remove
+    return sum(1 for _ in removed_rolls)
+
+
+def solve(puzzle_input: str) -> tuple[int, int]:
+    rolls_coords = set()
+    for y, line in enumerate(puzzle_input.splitlines()):
+        for x, char in enumerate(line):
+            if char == "@":
+                rolls_coords.add((x, y))
+    part_one = part1(rolls_coords)
+    part_two = part2(rolls_coords)
 
     return part_one, part_two
 
 
 if __name__ == "__main__":
-    run_puzzle(solve_function=solve, example=True)
+    run_puzzle(solve_function=solve, example=False)
