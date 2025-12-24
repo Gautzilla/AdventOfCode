50f4734e4ba454231aa83a2dcaa35d71afbdfac9
Gautzilla <gauthier.berthomieu@gmail.com>
2025-12-24 17:20:41 +0100

    add 2025 d10 p2

--- python_AoC/2025/10/2025_10.py
+++ python_AoC/2025/10/2025_10.py
@@ -1,8 +1,16 @@
 from template_creator import run_puzzle
 from collections import deque
+import z3
 
-def press_button(diagram: int, button: int) -> int:
+def press_light_button(diagram: int, button: int) -> int:
     return diagram^button
+
+def press_joltage_button(joltages: list[int], button:int) -> list[int]:
+    output = [*joltages]
+    for index in range(len(joltages)):
+        if 2**index & button:
+            output[len(output)-index-1] +=1
+    return output
 
 def parse_diagram(diagram: str) -> int:
     """Return the lit lights in the form of a base2 int."""
@@ -16,19 +24,18 @@ def parse_button(button: str, diagram_length: int) -> int:
     coords = list(map(int,button.split(",")))
     return sum(2**(diagram_length-c-1) for c in coords)
 
-def parse_line(line: str) -> tuple[int, list[int], str]:
+def parse_line(line: str) -> tuple[int, list[int], list[int]]:
     """Parse a manual line, returning the diagram, buttons and joltage requirements."""
     diagram, *buttons, joltage = line.split()
     buttons = [parse_button(button, len(diagram)-2) for button in buttons] # -2 to account for []
     diagram = parse_diagram(diagram)
-    primary_buttons = [button for button in buttons if button&diagram] # might switch target lights + extra lights
-    buttons = [button for button in buttons if any(b&button for b in primary_buttons)]
+    joltage = list(map(int,joltage.lstrip("{").rstrip("}").split(",")))
     return diagram, buttons, joltage
 
-def parse_input(input: str) -> list[tuple[int, list[int], str]]:
+def parse_input(input: str) -> list[tuple[int, list[int], list[int]]]:
     return list(map(parse_line, input.splitlines()))
 
-def min_nb_switch(target:int, buttons: list[int]) -> int:
+def min_nb_light_switch(target:int, buttons: list[int]) -> int:
     queue = deque([(0, 0)]) # status, depth, visited
     visited = set()
 
@@ -40,22 +47,37 @@ def min_nb_switch(target:int, buttons: list[int]) -> int:
             continue
         visited.add(diagram)
         for button in buttons:
-            next_diagram = press_button(diagram=diagram, button=button)
+            next_diagram = press_light_button(diagram=diagram, button=button)
             queue.append((next_diagram, depth+1))
 
+def solve_joltage(buttons: list[int], joltages: list[int]) -> int:
+    o = z3.Optimize()
+    nb_presses = z3.Ints(f"n{i}" for i in range(len(buttons)))
+    for press in nb_presses:
+        o.add(press >= 0)
+    for idx, joltage in enumerate(joltages[::-1]):
+        equation = 0
+        for b_idx, button in enumerate(buttons):
+            if not (2 ** idx & button):
+                continue
+            equation += nb_presses[b_idx]
+        o.add(equation == joltage)
+    o.minimize(sum(nb_presses))
+    o.check()
+    return o.model().eval(sum(nb_presses)).as_long()
 
-def part1(input: list[tuple[int, list[int], str]]) -> int:
-    return sum(min_nb_switch(target, buttons) for target,buttons,_ in input)
+def part1(input: list[tuple[int, list[int], list[int]]]) -> int:
+    return sum(min_nb_light_switch(target, buttons) for target,buttons,_ in input)
 
 
-def part2(puzzle_input: str) -> any:
-    return ""
+def part2(input: list[tuple[int, list[int], list[int]]]) -> any:
+    return sum(solve_joltage(buttons, joltages) for _,buttons,joltages in input)
 
 
 def solve(puzzle_input: str) -> tuple[any, any]:
     input = parse_input(puzzle_input)
     part_one = part1(input)
-    part_two = part2(puzzle_input)
+    part_two = part2(input)
 
     return part_one, part_two
 
